1e3a5aa5cd4efae38bf296d5ce7f82988eb6173d
mostafa-falcon <mostafa0falcon@gmail.com>
2025-07-01 21:02:52 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Mostafa Falcon',
   description: "I'm Mostafa Falcon, a mobile app developer using Flutter and a full-stack web developer with Next.js & TailwindCSS. I build high-performance digital products with great UX. Let's turn your idea into a successful app or website.",
+  icons: {
+    icon: 'myIcon.ico'
+  },
   openGraph: {
     title: 'Mostafa Falcon',
-  description: "I'm Mostafa Falcon, a mobile app developer using Flutter and a full-stack web developer with Next.js & TailwindCSS. I build high-performance digital products with great UX. Let's turn your idea into a successful app or website.",
-    url: 'https://mostafa-falcon.vercel.app/', // ضع دومين موقعك هنا
+    description: "I'm Mostafa Falcon, a mobile app developer using Flutter and a full-stack web developer with Next.js & TailwindCSS. I build high-performance digital products with great UX. Let's turn your idea into a successful app or website.",
+    url: 'https://mostafa-falcon.vercel.app', // ضع دومين موقعك هنا
     siteName: 'Mostafa Falcon',
     images: [
       {
