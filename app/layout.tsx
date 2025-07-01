c5141d83edc416d00e2d43cfd149a159209ef3b2
mostafa-falcon <mostafa0falcon@gmail.com>
2025-07-01 20:58:57 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,26 +9,24 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Mostafa Falcon',
   description: "I'm Mostafa Falcon, a mobile app developer using Flutter and a full-stack web developer with Next.js & TailwindCSS. I build high-performance digital products with great UX. Let's turn your idea into a successful app or website.",
-  icons: {
-    icon: '/myIcon.ico'
-  },
   openGraph: {
     title: 'Mostafa Falcon',
-    description: "I'm Mostafa Falcon, a mobile app developer using Flutter and a full-stack web developer with Next.js & TailwindCSS. I build high-performance digital products with great UX. Let's turn your idea into a successful app or website.",
+  description: "I'm Mostafa Falcon, a mobile app developer using Flutter and a full-stack web developer with Next.js & TailwindCSS. I build high-performance digital products with great UX. Let's turn your idea into a successful app or website.",
+    url: 'https://mostafa-falcon.vercel.app/', // ضع دومين موقعك هنا
+    siteName: 'Mostafa Falcon',
     images: [
       {
-        url: '/thumbnail.png',
+        url: '/thumbnail.png', // المسار النسبي للصورة
         width: 1200,
         height: 630,
-        alt: 'Mostafa Falcon - Mobile and Web Developer'
-      }
+        alt: 'Mostafa Falcon Portfolio',
+      },
     ],
     type: 'website',
     locale: 'en_US',
-    siteName: 'Mostafa Falcon',
-    url: 'https://mostafa-falcon.vercel.app/',
   },
 };
+
 
 export default function RootLayout({
   children,
